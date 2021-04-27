c287cd4e501c2aa63226113e8b240da804fa619e
chikara-dot <victorycastlepower0209@gmail.com>
2021-04-27 12:43:11 +0000

    3

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '287d664d42bf1898bcd2f1a093af51ccd744b371a1837ae766b89ac14f26f079bb07ea8c07607737a26d87b7243a10193296e9df5c7c214f53a31273d64c13ad'
